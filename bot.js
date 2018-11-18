ae9658160eed0ee07d80897743172539340625aa
sucukluekmekpeynir <44277051+sucukluekmekpeynir@users.noreply.github.com>
2018-11-18 17:15:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -478,4 +478,37 @@ if (msg.content.toLowerCase().match(/(discord\.gg\/)|(discordapp\.com\/invite\/)
     }
     });  
 
+const { Command } = require('discord.js-commando');
+const Discord = require('discord.js');
+
+module.exports = class ScanPlayingCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'reklam-taraması',
+            group: 'moderasyon',
+            aliases: ['reklam-taraması'],
+            memberName: 'reklam-taraması',
+            description: 'Oynuyor mesajlarındaki reklamları arar.',
+            examples: ['reklam-taraması'],
+            guildOnly: true,
+        })
+    }
+
+    hasPermission(msg) {
+        if(!msg.guild) return this.client.isOwner(msg.author);
+        return this.client.isOwner(msg.author) || msg.member.hasPermission("MANAGE_GUILD");
+    }
+
+    run(msg) {
+        const members = msg.guild.members.filter(member => member.user.presence.game && /(discord.gg|discord.me|discordapp.com|discord.io|discord.tk)/g.test(member.user.presence.game.name));
+        const embed = new Discord.RichEmbed()
+            embed.setDescription(members.map(member => `${member}`).join("\n") || "Kimsenin oynuyor mesajı reklam içermiyor.")
+            embed.setColor("RANDOM")
+            embed.setTimestamp()
+            embed.setFooter(`${this.client.user.username} Reklam Tarama Sistemi`)
+        msg.channel.send({embed})
+
+    }
+}
+
 client.login(process.env.BOT_TOKEN);
