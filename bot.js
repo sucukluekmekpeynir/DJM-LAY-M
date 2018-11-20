8b87eaba48ede48d978e3b702b05cc88e0e14878
sucukluekmekpeynir <44277051+sucukluekmekpeynir@users.noreply.github.com>
2018-11-20 17:10:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -388,7 +388,7 @@ description: ('Bu kanal NSFW kanalı değil.')
     client.on('message', async msg => {
     if (msg.content.toLowerCase() === prefix + "disko") {
    if (msg.channel.type === "dm") return;
-  const rol = 'Disko' // Rol ismi buraya
+  const rol = 'disko'
   setInterval(() => {
       msg.guild.roles.find(s => s.name === rol).setColor("RANDOM")
       }, 9000);
